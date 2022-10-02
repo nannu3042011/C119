34ad96eee86fe958c63cb3709845c10899b10a19
nannu3042011 <86117297+nannu3042011@users.noreply.github.com>
2022-10-02 16:46:34 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+  // https://teachablemachine.withgoogle.com/models/NcaePcwr_/ //
+
+Webcam.set({
+    width:350,
+    height:300,
+    image_format: 'png',
+    png_quality:90
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach( '#camera' );
